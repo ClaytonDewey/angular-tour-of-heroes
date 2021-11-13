3cfae8e66de25a5f272f8cc2ed4b3500e237fbed
ClaytonDewey <clay@dryadmedia.com>
2021-11-13 16:22:14 -0600

    Finished Message service & component
    Fixed a couple of typos as well...

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
     selector: 'app-heroes',
@@ -8,29 +10,27 @@ import { HeroService } from '../hero.service';
     styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
 
-    heroes: Hero[] = [];
+  selectedHero?: Hero;
 
-    selectedHero?: Hero;
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
+  heroes: Hero[] = [];
 
-    constructor(
-      private heroService: HeroService
+  constructor(
+    private heroService: HeroService
+    , private messageService: MessageService
     ) { }
+    
+  ngOnInit() {
+    this.getHeroes();
+  }
 
-    getHeroes(): void {
-      this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
-    }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
 
-    ngOnInit() {
-      this.getHeroes();
-    }
-
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 }
